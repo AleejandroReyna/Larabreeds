33e35c9abae399ef150aebf7f3fe9703ddb0e1f6
Alejandro Reyna <AleejandroReyna@gmail.com>
2021-04-09 23:47:15 -0600

    Seeder for create default categories

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
+use Ds\Set;
 
 class CategorySeeder extends Seeder
 {
@@ -13,6 +16,29 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = new Set();
+        $breeds = [];
+        $this->command->info('Get Available Breeds with Dog Ceo API: (https://dog.ceo/api/breeds/list/all).');
+        $request = Http::get('https://dog.ceo/api/breeds/list/all');
+        if($request->ok()) {
+            $this->command->info("Get Breeds finished!");
+            $response = $request->json();
+            $breeds = $response['message'];
+        
+            $this->command->info("Filtering categories...");
+            foreach($breeds as $breed => $content) {
+                if($content) {
+                    foreach($content as $item) {
+                        $categories->add($item);
+                    }
+                }
+            }
+            foreach($categories as $category) {
+                DB::table('categories')->insert([
+                    'name' => $category
+                ]);
+                $this->command->info("Category ${category} added..");
+            }
+        }
     }
 }
